f3330fbcc3aa1b8740c2b5af87f6aca5793c7232
ccd-utexas <ccd.utexas@gmail.com>
2013-09-27 16:32:09 -0500

    Using UNIXTIME to compare timestmaps. Good enough.

--- check_fits_timestamps.py
+++ check_fits_timestamps.py
@@ -7,6 +7,7 @@ exposure time. This program is specific to Argos data.
 
 Example use:
 check_fits_timestamps.py /path/to/data/A1234.*.fits
+Must call with 2 or more FITS files.
 """
 # TODO: Format documentation following
 # http://docs.python.org/devguide/documenting.html
@@ -14,7 +15,7 @@ check_fits_timestamps.py /path/to/data/A1234.*.fits
 import sys
 from astropy.io import fits
 import time
-import datetime
+import datetime as dt
 
 def main():
     """Main code.
@@ -29,13 +30,11 @@ def main():
     """
     # Main program is modularized to prevent code execution when being imported.
     stop_instructions = "Hit Ctrl-C to stop program."
-    print
-    print stop_instructions
     while True:
         check_fits_timestamps()
         print
         print stop_instructions
-        sleep_time = 10 # seconds
+        sleep_time = 60 # seconds
         print "Sleeping for", sleep_time, "seconds."
         time.sleep(sleep_time)
 
@@ -64,7 +63,7 @@ def check_fits_timestamps():
     Argos data.
 
     Arguments:
-    List of FITS files.
+    2 or more FITS files.
 
     Returns:
     Prints to stdout.
@@ -75,7 +74,12 @@ def check_fits_timestamps():
     fits_file_list = sys.argv[1:]
 
     print
-    print datetime.datetime.utcnow().isoformat()
+    if len(fits_file_list) < 2:
+        print "Must call with 2 or more FITS files."
+        print "Example use:"
+        print "check_fits_timestamps.py /path/to/data/A1234.*.fits"
+        sys.exit()
+    print dt.datetime.utcnow().isoformat()
     print "Begin checking FITS timestamps for "
     print fits_file_list[0]
     print "..."
@@ -88,15 +92,28 @@ def check_fits_timestamps():
     hdulist        = fits.open(old_fits_file)
     old_exptime    = float(hdulist[0].header['EXPTIME'])
     old_unixtime   = float(hdulist[0].header['UNIXTIME'])
+    # TODO: Combine UTC and DATE-OBS. Just report strings for now.
+    # old_utc        = dt.datetime.strptime(hdulist[0].header['UTC'], "%H:%M:%S")
+    # old_dateobs    = dt.datetime.strptime(hdulist[0].header['DATE-OBS'], "%Y-%m-%d")
+    old_utc        = hdulist[0].header['UTC']
+    old_dateobs    = hdulist[0].header['DATE-OBS']
     hdulist.close()
     for fits_file in fits_file_list[1:]:
         new_fits_file = fits_file
         hdulist       = fits.open(new_fits_file)
         new_exptime   = float(hdulist[0].header['EXPTIME'])
         new_unixtime  = float(hdulist[0].header['UNIXTIME'])
+        # TODO: Combine UTC and DATE-OBS. Just report strings for now
+        # new_utc       = dt.datetime.strptime(hdulist[0].header['UTC'], "%H:%M:%S")
+        # new_dateobs   = dt.datetime.strptime(hdulist[0].header['DATE-OBS'], "%Y-%m-%d")
+        new_utc        = hdulist[0].header['UTC']
+        new_dateobs    = hdulist[0].header['DATE-OBS']
         hdulist.close()
+        # Check that EXPTIMEs of both files is same.
         if new_exptime == old_exptime:
             exptime = old_exptime
+            # Compare Posix timestamps to EXPTIME test timing for now.
+            # TODO: Use datetime.deltatime to compare UTC and DATE-OBS.
             if new_unixtime - old_unixtime != exptime:
                 flag_no_timing_errors = False
                 print
@@ -105,24 +122,29 @@ def check_fits_timestamps():
                 print "new_UNIXTIME - old_UNIXTIME =", new_unixtime - old_unixtime
                 print "EXPTIME =", exptime
                 print "old_fits_file =", old_fits_file
-                print "old_UNIXTIME =", old_unixtime
+                print "old_UNIXTIME  =", old_unixtime
+                print "old_UTC       =", old_utc
+                print "old_DATE-OBS  =", old_dateobs
                 print "new_fits_file =", new_fits_file
-                print "new_UNIXTIME =", new_unixtime
-            # TODO: Add error checking for UTC, DATE-OBS using datetime module
-            # print hdulist[0].header['UTC']
-            # print hdulist[0].header['DATE-OBS']
+                print "new_UNIXTIME  =", new_unixtime
+                print "new_UTC       =", new_utc
+                print "new_DATE-OBS  =", new_dateobs
+        # Error if EXPTIMEs of two files is different.
         else:
             flag_no_timing_errors = False
             print
             print bcolors.BOLDRED + "TIMING ERROR: new_EXPTIME != old_EXPTIME" \
                   + bcolors.DEFAULT
             print "old_fits_file =", old_fits_file
-            print "EXPTIME =", old_exptime
+            print "EXPTIME       =", old_exptime
             print "new_fits_file =", new_fits_file
-            print "EXPTIME =", new_exptime
+            print "EXPTIME       =", new_exptime
+        # Set current "new" values to "old" in prep for comparison with next file.
         old_fits_file = new_fits_file
         old_exptime   = new_exptime
         old_unixtime  = new_unixtime
+        old_utc       = new_utc
+        old_dateobs   = new_dateobs
 
     print
     if flag_no_timing_errors:
@@ -130,7 +152,7 @@ def check_fits_timestamps():
     else:
         print bcolors.BOLDRED + "WARNING: Timing errors were found." + bcolors.DEFAULT
     print "End checking timestamps."
-    print datetime.datetime.utcnow().isoformat()
+    print dt.datetime.utcnow().isoformat()
 
 if __name__ == "__main__":
     try:
